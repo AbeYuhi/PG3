676e6bbbcd5c4752dd3f5dbb9f58c3ca15dbb682
AbeYuhi <k022g0032@g.neec.ac.jp>
2023-10-02 12:17:21 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 
+int HourlyWageCalculation(int workTime) {
+	return 1072 * workTime;
+}
+
+int HourlyWageCalculationRecurrence(int workTime, int money) {
+	if (workTime <= 1) {
+		return 0;
+	}
+	int Salary = money * 2 - 50;
+	HourlyWageCalculationRecurrence()
+}
+
 int main() {
 	
-	
+	for (int i = 0; i <= 100; i++) {
+		std::wcout << i << "ŽžŠÔ“­‚­ê‡" << std::endl;
+		std::wcout << "ŒÅ’è’l‚Ìê‡==>" << HourlyWageCalculation(i) << "‰~" << std::endl;
 
+		
+		std::cout << std::endl;
+	}
 	return 0;
 }
