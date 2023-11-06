89e51be5ab154ec6a0730535f13995dacc822416
AbeYuhi <k022g0032@g.neec.ac.jp>
2023-11-06 11:35:31 +0900

    完成

--- Comparison.h
+++ Comparison.h
@@ -4,11 +4,17 @@ template <typename T1, typename T2>
 class Comparison
 {
 public:
-	Comparison(T1 num1, T2 num2);
-	~Comparison();
+	Comparison(T1 num1, T2 num2) : num1_(num1), num2_(num2) {}
+	~Comparison() = default;
 
-	template <typename T1, typename T2>
-	auto Min() const -> decltype(std::declval(T1));
+	auto Min() -> typename std::common_type<T1, T2>::type
+	{
+		if (num1_ <= num2_) {
+			return static_cast<typename std::common_type<T1, T2>::type>(num1_);
+		}
+
+		return static_cast<typename std::common_type<T1, T2>::type>(num2_);
+	}
 
 private:
 	T1 num1_;
--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 
 int main() {
 	Comparison<int, int> com1(1, 2);
-	Comparison<float, float> com2(1.0f, 2.0f);
-	Comparison<double, double> com3(1.0, 2.0);
-	//Comparison<int, float> com4(1, 0.8f);
-	//Comparison<int, double> com5(1, 0.5);
-	//Comparison<float, double> com6(1.3f, 1.1);
+	Comparison<float, float> com2(2.0f, 3.0f);
+	Comparison<double, double> com3(4.0, 3.0);
+	Comparison<int, float> com4(1, 0.8f);
+	Comparison<int, double> com5(1, 0.5);
+	Comparison<float, double> com6(1.3f, 1.1);
 
-	printf("%d\n", com1.Min());
-	printf("%f\n", com2.Min());
-	printf("%lf\n", com3.Min());
+	std::cout << com1.Min() << std::endl;
+	std::cout << com2.Min() << std::endl;
+	std::cout << com3.Min() << std::endl;
+	std::cout << com4.Min() << std::endl;
+	std::cout << com5.Min() << std::endl;
+	std::cout << com6.Min() << std::endl;
 
 	return 0;
 }
