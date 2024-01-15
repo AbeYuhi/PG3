4cb7b05f24861317005a89beb5ede943beb2c622
AbeYuhi <k022g0032@g.neec.ac.jp>
2024-01-15 10:57:40 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include <thread>
+#include <string>
+
+void PrintfString(std::string string) {
+	printf("%s\n", string.c_str());
+}
 
 int main() {
-	
-	
+
+	std::thread th1(PrintfString, "thread 1");
+	th1.join();
+
+	std::thread th2(PrintfString, "thread 2");
+	th2.join();
+
+	std::thread th3(PrintfString, "thread 3");
+	th3.join();
 
 	return 0;
 }
