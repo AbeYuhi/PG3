9f5614865a878daf85d2bf17c3dd57247b810940
AbeYuhi <k022g0032@g.neec.ac.jp>
2024-01-29 12:00:25 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include <string>
+#include <chrono>
+#include <Windows.h>
 
 int main() {
 	
-	
+	std::string strA(100000, 'a');
+
+	std::chrono::steady_clock::time_point reference = std::chrono::steady_clock::now();
+	std::string strCpy = strA;
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	std::chrono::microseconds copyTime = std::chrono::duration_cast<std::chrono::microseconds>(now - reference);
+
+	reference = std::chrono::steady_clock::now();
+	std::string strMove = std::move(strA);
+	now = std::chrono::steady_clock::now();
+	std::chrono::microseconds moveTime = std::chrono::duration_cast<std::chrono::microseconds>(now - reference);
+
+	printf("100,000文字を移動とコピーで比較しました。\n");
+	std::cout << "コピー : " << copyTime.count() << "μs" << std::endl;
+	std::cout << "移動 : " << moveTime.count() << "μs" << std::endl;
+
+	system("PAUSE");
 
 	return 0;
 }
